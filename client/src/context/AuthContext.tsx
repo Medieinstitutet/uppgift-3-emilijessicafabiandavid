46563c8aed288eac39f25bcdcb8f8d2f046a2b41
screamm <davidrydgren@gmail.com>
2024-06-11 14:59:41 +0200

    fortfarande fel

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -60,17 +60,6 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     localStorage.setItem("stripeSessionId", stripeId);
   };
 
-  // const login = (user: User, stripeId: string) => {
-  //   const isAuthenticated = !!stripeId && !!user;
-  //   if (!isAuthenticated) {
-  //     setUser(user);
-  //     setStripeSessionId(stripeId);
-  //     console.log("User logged in:", user);
-  //     console.log("Stripe Session ID:", stripeSessionId);
-  //     localStorage.setItem("user", JSON.stringify(user));
-  //     localStorage.setItem("stripeSessionId", stripeId);
-  //   }
-  // };
 
   const logout = async () => {
     try {
